1ea4331d8a19b7e44419500c73ad16c431139a38
Kim-777 <iloo3012@naver.com>
2021-03-02 16:03:39 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+//application/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 분석
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://ariwarabbang:asdf1234@boilerplate-shard-00-00.fhnli.mongodb.net:27017,boilerplate-shard-00-01.fhnli.mongodb.net:27017,boilerplate-shard-00-02.fhnli.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-5qnlri-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
 .then(() => console.log('MongoDB Connected...'))
@@ -11,6 +23,22 @@ mongoose.connect('mongodb://ariwarabbang:asdf1234@boilerplate-shard-00-00.fhnli.
 
 
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세여!!'));
+app.get('/', (req, res) => res.send('Hello World! 안녕하세여!! ㅎㅎ 저는 초보에염'));
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 데이터를 데이터 베이스에 넣어줍니다.
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
